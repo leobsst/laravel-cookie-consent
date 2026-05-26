99bebc9abaca444172fca6648458eaeae6e55c7b
LEOBSST <contact@leobsst.fr>
2026-05-26 15:28:02 +0200

    ✨ feat: expose hasAdSense, hasGtm, hasPosthog flags in window.CookieConsent
    Blade components now set hasAdSense, hasGtm, and hasPosthog on the
    window.CookieConsent config object so cookie-consent.js can conditionally
    activate only the features relevant to the current page setup.

--- resources/views/components/scripts.blade.php
+++ resources/views/components/scripts.blade.php
@@ -4,7 +4,13 @@
 @if($googleAdsenseClientId || $googleTagManagerId || $posthogProjectToken)
 @once
         <script src="{{ asset('vendor/cookie-consent/cookie-consent.js') }}"></script>
-        <script>window.__cookieConsentStatus = @js($cookieConsentStatus === 'full' ? 'granted' : 'denied');</script>
+        <script>
+            window.__cookieConsentStatus = @js($cookieConsentStatus === 'full' ? 'granted' : 'denied');
+            window.CookieConsent = window.CookieConsent || {};
+            window.CookieConsent.hasGtm = {{ $googleTagManagerId ? 'true' : 'false' }};
+            window.CookieConsent.hasAdSense = {{ $googleAdsenseClientId ? 'true' : 'false' }};
+            window.CookieConsent.hasPosthog = {{ $posthogProjectToken ? 'true' : 'false' }};
+        </script>
 @endonce
 @endif
 @if($googleAdsenseClientId && !$googleTagManagerId)
